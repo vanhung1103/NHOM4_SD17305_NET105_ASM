9dd025a98bdc503a438405fc55143163d00c59b7
Trung27012003 <trung27012003@gmail.com>
2023-06-08 23:13:11 +0700

    add

---
+++ NHOM5_NET105_SD17305/NHOM5_NET105_SD17305/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NHOM5_NET105_SD17305.Views.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
