db17a2ea0570b94190581efcc1f237e995f230c8
DESKTOP-VVKT5NE\dinht <dinhthien18052003@gmail.com>
2023-06-09 12:23:20 +0700

    thienv3dz

--- NHOM5_NET105_SD17305/NHOM5_NET105_SD17305/Program.cs
+++ NHOM5_NET105_SD17305/NHOM5_NET105_SD17305/Program.cs
@@ -1,3 +1,5 @@
+using AspNetCoreHero.ToastNotification;
+using AspNetCoreHero.ToastNotification.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NHOM5_NET105_SD17305.Data.Data;
@@ -5,7 +7,7 @@ using NHOM5_NET105_SD17305.Data.IServices;
 using NHOM5_NET105_SD17305.Data.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddNotyf(config => { config.DurationInSeconds = 3; config.IsDismissable = true; config.Position = NotyfPosition.TopRight; });
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<FastFoodDbContext>(options =>
@@ -42,7 +44,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseNotyf();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseSession();
