2b95530ba71dba8c0b62b7dbe55adc79bd06f50e
Trung27012003 <trung27012003@gmail.com>
2023-06-11 16:04:50 +0700

    xoa file rac

--- NHOM5_NET105_SD17305/TestLogin/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TestLogin.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
