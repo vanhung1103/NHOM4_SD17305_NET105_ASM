02eff4d99bcfdfa4e97092007afa9c004a74ac8a
DESKTOP-VVKT5NE\dinht <dinhthien18052003@gmail.com>
2023-06-12 15:51:16 +0700

    profile xaudcd

---
+++ NHOM5_NET105_SD17305/NHOM5_NET105_SD17305/Areas/Admin/Controllers/ProfileAdminController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NHOM5_NET105_SD17305.Data.IServices;
+
+namespace NHOM5_NET105_SD17305.Views.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProfileAdminController : Controller
+    {
+        private readonly IUserServices _userService;
+        private readonly ICustomerServices _customerService;
+        private readonly IAddressServices _addessService;
+
+        public ProfileAdminController(IUserServices userServices, ICustomerServices customerServices, IAddressServices addressServices)
+        {
+            _userService = userServices;
+            _customerService = customerServices;
+            _addessService = addressServices;
+        }
+        public async Task<IActionResult> IndexAdmin()
+        {
+            int isUser = Convert.ToInt32(HttpContext.Session.GetString("UserId"));
+            var a = await _customerService.GetCustomerByIdAsync(isUser);
+            if (a == null)
+            {
+                return RedirectToAction("Index", "Home", new { area = "Admin" });
+            }
+            return View(a);
+        }
+
+        public async Task<IActionResult> Edit()
+        {
+            int isUser = Convert.ToInt32(HttpContext.Session.GetString("UserId"));
+            var a = await _customerService.GetCustomerByIdAsync(isUser);
+            return View(a);
+        }
+
+    } 
+}
---
+++ NHOM5_NET105_SD17305/NHOM5_NET105_SD17305/Areas/Customer/Controllers/ProfileController.cs
@@ -0,0 +1,41 @@
+﻿    using Microsoft.AspNetCore.Mvc;
+using NHOM5_NET105_SD17305.Data.IServices;
+using NHOM5_NET105_SD17305.Data.Models;
+using System.Security.AccessControl;
+
+namespace NHOM5_NET105_SD17305.Views.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ProfileController : Controller
+    {
+        private readonly IUserServices _userService;
+        private readonly ICustomerServices _customerService;
+        private readonly IAddressServices _addessService;
+
+        public ProfileController(IUserServices userServices,ICustomerServices customerServices,IAddressServices addressServices)
+        {
+            _userService = userServices;
+            _customerService = customerServices;
+            _addessService = addressServices;
+        }
+        public async Task<IActionResult> Index()
+        {
+            int isUser = Convert.ToInt32(HttpContext.Session.GetString("UserId"));
+            var a = await _customerService.GetCustomerByIdAsync(isUser);
+            if (a == null)
+            {
+                return RedirectToAction("Home", "Home", new {area="Customer"});
+            }
+            return View(a);
+        }
+
+        public async Task<IActionResult> EditUser()
+        {
+            int isUser = Convert.ToInt32(HttpContext.Session.GetString("UserId"));
+            var a = await _customerService.GetCustomerByIdAsync(isUser);
+            return View(a);
+        }
+       
+
+    }
+}
